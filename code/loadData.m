73e8b3210c7ca3ebf0a9a36994a9a716d1f78e5f
obi <ole.bialas@posteo.de>
2022-09-27 09:14:57 -0400

    dont annex text

--- code/loadData.m
+++ code/loadData.m
@@ -1,1 +1,76 @@
-../.git/annex/objects/ZQ/84/MD5E-s1861--14598e1efea70c96372b7862863aa2c1.m/MD5E-s1861--14598e1efea70c96372b7862863aa2c1.m
+function [stim,resp]=loadData(sub,dset,dur,skip,nrm,fs,env,spg,ons,pho)
+
+arguments
+	sub char
+	dset char
+	dur double = 5
+	skip double = 1
+	nrm logical = false
+	fs double = 64
+	env logical = true
+	spg logical = false
+	ons logical = false
+	pho logical = false
+end
+
+eegDir = getenv('EEGDIR');
+stimDir = getenv('STIMDIR');
+if isempty(eegDir) | isempty(stimDir)
+	error 'Could not find environment variables $EEGDIR and/or $STIMDIR ...'
+end
+
+subDir = fullfile(eegDir, dset, sub);
+eegFiles = dir(fullfile(subDir, '/*run*.mat'));
+
+resp = {};
+stim = {};
+for ii = 1:length(eegFiles)-1
+	eeg = load(fullfile(...
+		eegFiles(ii).folder, eegFiles(ii).name));
+	eegData = eeg.eegData';  % TODO: store the data as times x channels
+	if skip
+		eegData = eegData(skip*eeg.fs:end, :)
+	end
+	sft = load(fullfile(...
+		stimDir, dset, join(['audio', eegFiles(ii).name(10:11), '.mat'])));	
+	% TODO: filtering, resampling	
+	if nrm
+		eegData = (eegData-mean(eegData))./std(eegData);
+		sft.env = sft.env./mean(sft.env.^2)^0.5;
+		sft.spg = sft.env./mean(sft.spg.^2)^0.5;
+	feats = {};
+	if env
+		feats = [feats; sft.env];
+	end
+	if ons
+		feats = [feats; sft.ons];
+	end
+	if spg
+		feats = [feats; sft.spg];
+	end
+	if pho
+		feats = [feats; sft.pho];
+	end
+	feats = cat(2, feats{:}); % concatenate to one matrix
+	if length(feats) < length(eegData)
+		eegData = eegData(1:length(feats),:);
+	else
+		feats = feats(1:length(eegData),:);
+	end
+	% crop the data into segments of length 'dur'	
+	toCut = mod(length(eegData), dur*fs);
+	eegData = eegData(1:length(eegData)-toCut, :);
+	feats = feats(1:length(feats)-toCut, :);
+	nTrials = length(eegData) / (dur*fs);
+	eegData = permute(reshape(eegData,[dur*fs, nTrials, 128]),[2 1 3]);
+	feats = permute(reshape(feats, dur*fs, nTrials, 1), [2 1 3]);
+	for it=1:nTrials
+		resp=[resp; squeeze(eegData(it, :, :))];
+		stim=[stim; squeeze(env(it, :, :))];
+	end
+end
+
+end
+
+
+
--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,1 +1,59 @@
-../.git/annex/objects/1z/98/MD5E-s2455--1de5bca1c9ede9ed0bc3283f938d7aff.py/MD5E-s2455--1de5bca1c9ede9ed0bc3283f938d7aff.py
+from pathlib import Path
+import numpy as np
+from scipy.io import loadmat, savemat
+from mne.io import RawArray
+from mne import create_info
+from mne.channels import make_standard_montage
+from mne.preprocessing import ICA, read_ica, corrmap
+from pyprep.ransac import find_bad_by_ransac
+from meegkit.dss import dss_line
+from meegkit.detrend import detrend
+
+root = Path(__file__).parent.parent.absolute()
+data_dir = root / "input" / "raw"
+out_dir = root / "input" / "preprocessed"
+montage = make_standard_montage("biosemi128")
+info = create_info(ch_names=montage.ch_names, sfreq=128, ch_types="eeg")
+reference_ica = read_ica(root / "code" / "reference-ica.fif")
+for sub_folder_in in data_dir.glob("sub*"):
+    sub_folder_out = out_dir / sub_folder_in.name
+    if not sub_folder_out.exists():
+        sub_folder_out.mkdir()
+    for matfile in sub_folder_in.glob("*.mat"):
+        mat = loadmat(matfile)
+        # Reference to mastoids average
+        data = mat["eegData"] - mat["mastoids"].mean(axis=1, keepdims=True)
+        data /= 1e6  # convert to V
+        data = np.expand_dims(data, axis=2)
+        # remove power line noise
+        data, _ = dss_line(data, fline=50, sfreq=info["sfreq"], nremove=5)
+        data, _, _ = detrend(data, 5)
+        raw = RawArray(data.squeeze().T, info)
+        raw.set_eeg_reference([])  # mark data as referenced
+        raw.set_montage(montage)
+        # blink removal with ICA
+        ica = ICA(n_components=100)
+        ica.fit(raw)
+        corrmap([reference_ica, ica], template=(0, 0), show=False, label="blinks")
+        raw = ica.apply(raw, exclude=ica.labels_["blinks"])
+        bads, _ = find_bad_by_ransac(
+            raw.get_data(),
+            sample_rate=128,
+            exclude=[],
+            complete_chn_labs=np.array(raw.info["ch_names"]),
+            chn_pos=np.stack([r["loc"][0:3] for r in raw.info["chs"]]),
+        )
+        raw.info["bads"] = bads
+        raw = raw.interpolate_bads()
+        # save results
+        raw.save(sub_folder_out / f"{matfile.name[:-4]}-raw.fif", overwrite=True)
+        ica.save(sub_folder_out / f"{matfile.name[:-4]}-ica.fif", overwrite=True)
+
+        mat = {
+            "eegData": raw.get_data().T,
+            "chNames": raw.info["ch_names"],
+            "reference": "linked mastoids",
+            "fs": raw.info["sfreq"],
+            "chCoords": np.stack([r["loc"][0:3] for r in raw.info["chs"]]),
+        }
+        savemat(sub_folder_out / matfile.name, mat)
